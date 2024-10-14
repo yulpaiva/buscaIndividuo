aa0b22acd8b4252fea6dfe8c9a615aea3a838728
Yul Brynner <yulbrynner73@gmail.com>
2024-10-14 19:08:52 -0300

    first commit

--- server.js
+++ server.js
@@ -8,24 +8,6 @@ const Pokemon = require('./models/Pokemon'); // Modelo do MongoDB
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Verifica se a pasta 'uploads' existe, caso contrário, cria a pasta
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
-
-// Configuração do Multer para o upload de arquivos
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    },
-});
-
-const upload = multer({ storage });
-
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokedex', {
     useNewUrlParser: true,
@@ -38,42 +20,21 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokedex',
 
 // Middleware para JSON
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // Serve os arquivos de imagem da pasta 'uploads'
+
+// Rota para a raiz
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à API Pokédex! Use /api/pokemons para acessar os pokémons.');
+});
 
 // Rotas para CRUD
 app.get('/api/pokemons', async (req, res) => {
-    try {
-        const pokemons = await Pokemon.find();
-        res.json(pokemons);
-    } catch (error) {
-        console.error('Erro ao carregar Pokémons:', error);
-        res.status(500).json({ message: 'Erro ao carregar Pokémons' });
-    }
+    const pokemons = await Pokemon.find();
+    res.json(pokemons);
 });
 
-// Rota POST para cadastrar um novo Pokémon
-app.post('/api/pokemons', upload.single('image'), async (req, res) => {
-    const { name, type, description, height, weight, abilities, baseExperience } = req.body;
-    const imageUrl = req.file ? req.file.path : null; // Pega o caminho da imagem, se existir
-
-    try {
-        const pokemon = new Pokemon({
-            name,
-            type,
-            description,
-            imageUrl,
-            height,
-            weight,
-            abilities: abilities ? abilities.split(',').map((ability) => ability.trim()) : [],
-            baseExperience,
-        });
-
-        await pokemon.save();
-        res.status(201).json(pokemon); // Retorna o Pokémon criado com status 201
-    } catch (error) {
-        console.error('Erro ao cadastrar Pokémon:', error);
-        res.status(500).json({ message: 'Erro ao cadastrar Pokémon' });
-    }
+// Rota para adicionar um Pokémon
+app.post('/api/pokemons', async (req, res) => {
+    // Seu código para adicionar um Pokémon
 });
 
 // Inicializando o servidor
